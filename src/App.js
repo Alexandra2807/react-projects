94ab27294736ed3b19fcc4895217adc074723044
Alexandra2807 <30619563+Alexandra2807@users.noreply.github.com>
2020-02-14 14:16:10 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      languages: [
+        { name: "JavaScript", votes: 0 },
+        { name: "Python", votes: 0 },
+        { name: "C#", votes: 0 },
+        { name: "Java", votes: 0 }
+      ]
+    };
+  }
+
+  vote(i) {
+    let newLanguages = [...this.state.languages];
+    newLanguages[i].votes++;
+    function swap(array, i, j) {
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    this.setState({ languages: newLanguages });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Vote Your Favourite Language!</h1>
+        <div className="languages">
+          {this.state.languages.map((lang, i) => (
+            <div key={i} className="language">
+              <div className="voteCount">{lang.votes}</div>
+              <div className="languageName">{lang.name}</div>
+              <button onClick={this.vote.bind(this, i)}>Click Here</button>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+}
+export default App;
